fd2228d19b7448ecb392f7e8de0166bab26f74ca
Eturu Stephen <40745192+gamesonwalls@users.noreply.github.com>
2020-10-19 13:32:49 +0000

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,10 +16,15 @@ const PORT= process.env.PORT || 5000;
 
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
 
-app.get('/',function (req, res) {
-    res.send('API RUNINNING');
-
-})
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 app.post('/api/savePost',function (req, res) {
 
     //res.send(req.body)
