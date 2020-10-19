e21cbb0d9ac62b94a28207d8daa780cdc9332112
Eturu Stephen <40745192+gamesonwalls@users.noreply.github.com>
2020-10-19 13:45:41 +0000

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,11 +18,11 @@ app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
     
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 app.post('/api/savePost',function (req, res) {
