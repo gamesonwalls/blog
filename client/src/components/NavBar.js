bbb43717d8ecdcffced5d94de1011d447fd790a3
Eturu Stephen <40745192+gamesonwalls@users.noreply.github.com>
2020-10-21 12:40:18 +0000

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,49 +11,88 @@ function NavBar() {
     console.log("isLogin State from Navbar",isLogin)
     return(
      <div className="">
-                <nav  className="navbar navbar-default" style={{padding: 0,backgroundColor:'white'}}>
-          <div  className="container-fluid">
 
+      <header className="header">
+                  <div className="header__content row">
 
+                      <div className="header__logo">
+                          <a className="logo" href="index.html">
+                              <img src="images/logo.png" alt="Homepage" style={{width: 400,height: 76}}/>
+                          </a>
+                      </div> 
 
-            <div  className="navbar-header">
-              <button type="button"  className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span  className="sr-only">Toggle navigation</span>
-                <span  className="icon-bar"></span>
-                <span  className="icon-bar"></span>
-                <span  className="icon-bar"></span>
-              </button>
-            </div>
+                      <ul className="header__social">
+                          <li>
+                              <a href="#0"><i className="fa fa-facebook" aria-hidden="true"></i></a>
+                          </li>
+                          <li>
+                              <a href="#0"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+                          </li>
+                          <li>
+                              <a href="#0"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+                          </li>
+                          <li>
+                              <a href="#0"><i className="fa fa-pinterest" aria-hidden="true"></i></a>
+                          </li>
+                      </ul> 
 
-          
-            <div  className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            
-              {isLogin===false &&  
-              <ul  className="nav navbar-nav navbar-right">
-                <li><a style={{color:'black'}}>Not Logged in</a></li>
-                <li  className="">
-                   <Link to="/Login" style={{color:'black'}}>Sign In</Link>
-                </li>
-              </ul>
-              } 
+                      <a className="header__search-trigger" href="#0"></a>
 
-              {isLogin===true && 
-                <ul  className="nav navbar-nav navbar-right">
-                    <li><a style={{color:'black'}}><span className="badge badge-secondary">Welcome</span> {userName}</a></li>
-                    <li  className="dropdown">
-                      <a   className="dropdown-toggle"  style={{color:'black'}} data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account <span  className="caret"></span></a>
-                      <ul  className="dropdown-menu">
-                       
-                        <li><a onClick={handleLogout}>Logout</a></li>
-                      
+                      <div className="header__search">
+
+                          <form role="search" method="get" className="header__search-form" action="#">
+                              <label>
+                                  <span className="hide-content">Search for:</span>
+                                  <input type="search" className="search-field" placeholder="Type Keywords" value="" name="s" title="Search for:" autocomplete="off"/>
+                              </label>
+                              <input type="submit" className="search-submit" value="Search"/>
+                          </form>
+              
+                          <a href="#0" title="Close Search" className="header__overlay-close">Close</a>
                         
-                      </ul>
-                    </li>
-              </ul>
-              }
-            </div>
-          </div>
-        </nav>
+                      </div> 
+
+
+                      <a className="header__toggle-menu" href="#0" title="Menu"><span>Menu</span></a>
+
+                      <nav className="header__nav-wrap">
+
+                          <h2 className="header__nav-heading h6">Site Navigation</h2>
+
+                          <ul className="header__nav">
+                              <li className="current"><a href="index.html" title="">Home</a></li>
+                              <li className="has-children">
+                                  <a href="#0" title="">Categories</a>
+                                  <ul className="sub-menu">
+                                  <li><a href="category.html">Lifestyle</a></li>
+                                  <li><a href="category.html">Health</a></li>
+                                  <li><a href="category.html">Family</a></li>
+                                  <li><a href="category.html">Management</a></li>
+                                  <li><a href="category.html">Travel</a></li>
+                                  <li><a href="category.html">Work</a></li>
+                                  </ul>
+                              </li>
+                              <li className="has-children">
+                                  <a href="#0" title="">Blog</a>
+                                  <ul className="sub-menu">
+                                  <li><a href="single-video.html">Video Post</a></li>
+                                  <li><a href="single-audio.html">Audio Post</a></li>
+                                  <li><a href="single-gallery.html">Gallery Post</a></li>
+                                  <li><a href="single-standard.html">Standard Post</a></li>
+                                  </ul>
+                              </li>
+                              <li><a href="style-guide.html" title="">Styles</a></li>
+                              <li><a href="about.html" title="">About</a></li>
+                              <li><a href="contact.html" title="">Contact</a></li>
+                          </ul>
+
+                          <a href="#0" title="Close Menu" className="header__overlay-close close-mobile-menu">Close</a>
+
+                      </nav> 
+
+                  </div> 
+              </header> 
+          
     </div>
     )
 
